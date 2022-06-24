810af0de3092f97d73aa4f7fb8849bee77dd6072
Steven Kwok <derekkwok2020@gmail.com>
2022-06-24 12:18:41 +0100

    Not completed the lecture

--- HappyLlamasTest.java
+++ HappyLlamasTest.java
@@ -1,7 +1,7 @@
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class HappyLlamasTest {
 
@@ -14,6 +14,21 @@ class HappyLlamasTest {
 
     @Test
     public void testNormalTrampoline10(){
+        // ARRANGE - for simple methods, this mens setting up the parameters
+        boolean isNasaFabric = false;
+        int numTrampolines = 10;
 
+        // ACT - for simple methods, this generally means calling the method under test
+        // and then capturing its return to assert on
+        boolean result = HappyLlamas.areTheLlamasHappy(isNasaFabric, numTrampolines);
+
+        // ASSERT - basically just a conditional that proves the result is what
+        // you expect it to be, plus an extra message to display if it doesn't match.
+        //
+        // There are a wide variety of assert types, here we
+        // just want to assert that it returned false. But we could have also used
+        // assertEquals and passed in a false value.
+
+        assertFalse( result, "10 Llamas w/ Normal Trampolines should be Unhappy!" );
     }
 }
