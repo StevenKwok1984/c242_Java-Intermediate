5e305b96eda911cfc4606973a50a97640b3345c0
Steven Kwok <derekkwok2020@gmail.com>
2022-06-24 09:52:34 +0100

    Update HappyLlamasTest.java

--- HappyLlamasTest.java
+++ HappyLlamasTest.java
@@ -1,13 +1,19 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class HappyLlamasTest {
-    public HappyLlamasTest() {
+import static org.junit.jupiter.api.Assertions.*;
 
+class HappyLlamasTest {
+
+    public HappyLlamasTest() {
     }
 
     @org.junit.jupiter.api.Test
     public void testSomeMethod() {
-        // ARRANGE - for simple methods, this
+    }
+
+    @Test
+    public void testNormalTrampoline10(){
+
     }
 }
